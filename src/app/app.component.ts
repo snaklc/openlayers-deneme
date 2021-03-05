e694338350e6d14a623b641a7754a769c033bc68
snaklc <senakilic3@gmail.com>
2021-03-05 17:50:01 +0300

    araç tipleri eklendi

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,93 +1,58 @@
 import { Map, View } from 'ol';
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import VectorLayer from 'ol/layer/Vector';
 import VectorSource from 'ol/source/Vector';
 import { Style, Fill, Stroke, Text, Icon } from 'ol/style';
 import ImageLayer from 'ol/layer/Image';
 import Static from 'ol/source/ImageStatic';
-import {getCenter} from 'ol/extent';
-import {Circle as CircleStyle} from 'ol/style';
+import { getCenter } from 'ol/extent';
+import { Circle as CircleStyle } from 'ol/style';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  @ViewChild('map', { static: true }) mapRef: ElementRef;
   title = 'deneme';
-
-  private vectorLayer: VectorLayer;
-  private imageLayer: ImageLayer;
+  imageLayer: ImageLayer;
   map: Map;
   selectedType;
+  carClicked = false;
 
   constructor() {
   }
 
-  ngOnInit(){
+  ngOnInit() {
     this.initMap();
-    console.log(this.selectedType);
-    
+
   }
-  
-
   initMap() {
-
-    const extent = [0, 0, 1760, 1000];
-    // const projection = new Projection({
-    //   // code: 'xkcd-image',
-    //   units: 'pixels',
-    //   extent: extent,
-    // });
-     this.imageLayer =  new ImageLayer({
+    this.imageLayer = new ImageLayer({
       source: new Static({
-        // attributions: '© <a href="http://xkcd.com/license.html">xkcd</a>',
         url: '/assets/OrnekKavsak.jpg',
-        // projection: projection,
-        imageExtent: extent,
+        imageExtent: [0, 0, 1760, 1000],
       }),
     })
 
-    this.vectorLayer = new VectorLayer({
-      source: new VectorSource(),
-      style: new Style({
-        fill: new Fill({
-          color: 'rgba(255, 255, 255, 0.2)',
-        }),
-        stroke: new Stroke({
-          color: '#ffcc33',
-          width: 2,
-        }),
-        image: new CircleStyle({
-          radius: 7,
-          fill: new Fill({
-            color: '#ffcc33',
-          }),
-        }),
-      }),
-    });
-
-     this.map = new Map({
-      layers: [this.imageLayer, this.vectorLayer],
-      target: 'map',
+    this.map = new Map({
+      layers: [this.imageLayer],
+      target: this.mapRef.nativeElement,
       controls: [], // zoomIn zoomOut buttonlarını gizler
-      interactions:[], //image'in hareket etmemesini saglar
+      interactions: [], //image'in hareket etmemesini saglar
       view: new View({
         resolution: 1,        // important for 100% image size!
         maxResolution: 2,     // must be >= 1
         // projection: projection,
-        center: getCenter(extent),
+        center: getCenter([0, 0, 1760, 1000]),
         zoom: 0,
         maxZoom: 0,
-
       }),
     });
-
-    
-
   }
 
-  
-
- 
+  carSelected() {
+    this.carClicked = !this.carClicked;
+  }
 }
